25a215dbfa77f30aa950679eca0d8326d58ff455
HZERR <vadimvyazloy@yandex.ru>
2022-02-23 03:11:35 +0300

    Дополнена информация

--- src/main/java/ru/hzerr/LolilandInjectController.java
+++ src/main/java/ru/hzerr/LolilandInjectController.java
@@ -22,7 +22,6 @@ import java.util.ResourceBundle;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
